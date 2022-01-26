301768563adb977862128250228b0d2dcbb20b1e
이재석 <dlwotjr2@gmail.com>
2022-01-26 06:32:15 +0000

    [Fix] #4 / Key 추가, hr 변경

--- frontend/src/components/PostsList.tsx
+++ frontend/src/components/PostsList.tsx
@@ -7,13 +7,13 @@ import { postsListInfo } from "../data/dummy";
 
 export default function PostsList() {
   const [postsList, setPostsList] = useState<IPostsList[]>([]);
-  function getPostsList() {
-    // 임시 더미 데이터
-    // API GET /posts 통해 게시글 리스트 정보 받아올 예정
-    setPostsList(postsListInfo);
-  }
 
   useEffect(() => {
+    function getPostsList() {
+      // 임시 더미 데이터
+      // API GET /posts 통해 게시글 리스트 정보 받아올 예정
+      setPostsList(postsListInfo);
+    }
     getPostsList();
   }, []);
 
@@ -21,15 +21,12 @@ export default function PostsList() {
     <div className="posts-list">
       {postsList.map(({ id, order, category, title, author }) => {
         return (
-          <>
-            <Link className="posts-list__post" to={`/posts/${id}`}>
-              <div className="post__order">{order}</div>
-              <div className="post__category">{category}</div>
-              <div className="post__title">{title}</div>
-              <div className="post__author">{author}</div>
-            </Link>
-            <hr className="post__hr" />
-          </>
+          <Link key={id} className="posts-list__post" to={`/posts/${id}`}>
+            <div className="post__order">{order}</div>
+            <div className="post__category">{category}</div>
+            <div className="post__title">{title}</div>
+            <div className="post__author">{author}</div>
+          </Link>
         );
       })}
     </div>
