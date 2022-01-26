5c095de63ddb093c7ba26b55763dd395ac19569b
이재석 <dlwotjr2@gmail.com>
2022-01-26 04:59:00 +0000

    [Add] 게시글 리스트형 컴포넌트 추가

---
+++ frontend/src/@types/PostsList.d.ts
@@ -0,0 +1,7 @@
+export interface IPostsList {
+  id: number;
+  order: number;
+  category: string;
+  title: string;
+  author: string;
+}
---
+++ frontend/src/data/dummy.ts
@@ -0,0 +1,16 @@
+export const postsListInfo = [
+  {
+    id: 123,
+    order: 1,
+    category: "주제",
+    title: "제목",
+    author: "익명의 도도새",
+  },
+  {
+    id: 124,
+    order: 2,
+    category: "주제",
+    title: "제목",
+    author: "익명의 도도새",
+  },
+];
