5f22ce8a3e5b5797129f891a63163e7e0cc9cf89
장지원 <scorpiusjwj@gmail.com>
2022-01-27 09:57:03 +0000

    [Add] 좋아요 기능 및 async 더미데이터 사용하여 렌더링

---
+++ frontend/src/@types/SelectedPost.d.ts
@@ -0,0 +1,10 @@
+interface SelectedPost {
+  _id: string;
+  title: string;
+  author: string | "anonymity";
+  createdAt: string;
+  //더미데이터 삭제 후 Date로 변경 필요
+  likes: number;
+  view: number;
+  contents: string;
+}
