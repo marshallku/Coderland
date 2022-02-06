12c4cb36017fb5d220f4bf146c7f3a3103869832
구영표 <marshallkelice@gmail.com>
2022-02-06 23:28:36 +0000

    [Update] 마크다운 뷰어 업데이트

--- frontend/src/components/MarkdownViewer.tsx
+++ frontend/src/components/MarkdownViewer.tsx
@@ -1,17 +1,55 @@
 import ReactMarkdown from "react-markdown";
 import RehypeRaw from "rehype-raw";
-import RehypeSanitize from "rehype-sanitize";
+import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
+import remarkGfm from "remark-gfm";
+import { Prism } from "react-syntax-highlighter";
+import {
+  vscDarkPlus,
+  solarizedlight,
+} from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function MarkdownViewer({
   className,
   value,
 }: IMarkdownViewerProps) {
+  const sanitize = () =>
+    rehypeSanitize({
+      ...defaultSchema,
+      attributes: {
+        ...defaultSchema.attributes,
+        code: [...(defaultSchema?.attributes?.code || []), ["className"]],
+      },
+    });
   return (
     <ReactMarkdown
       className={className || ""}
-      rehypePlugins={[RehypeRaw, RehypeSanitize]}
+      remarkPlugins={[remarkGfm]}
+      rehypePlugins={[RehypeRaw, sanitize]}
       components={{
         h1: "h2",
+        // eslint-disable-next-line react/no-unstable-nested-components
+        code({ inline, className: childClassName, children }) {
+          const match = /language-(\w+)/.exec(childClassName || "");
+
+          return !inline && match ? (
+            <Prism
+              style={
+                // TODO: theme 상태관리 추가해 리렌더링
+                document.documentElement.classList.contains("dark")
+                  ? vscDarkPlus
+                  : solarizedlight
+              }
+              language={match[1]}
+              PreTag="div"
+            >
+              {String(children).replace(/\n$/, "")}
+            </Prism>
+          ) : (
+            <code className={childClassName} data-tmp={`${inline}`}>
+              {children}
+            </code>
+          );
+        },
       }}
     >
       {value ?? ""}
