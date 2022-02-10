0feb2dc3f6eaac75d2832d9973d11327e374aae4
정세영 <wjdtp93@gmail.com>
2022-02-10 11:52:48 +0000

    [Fix] 댓글 관련 로직 정리

--- backend/tests/comments.test.ts
+++ backend/tests/comments.test.ts
@@ -112,7 +112,7 @@ describe("댓글 통합 테스트", () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body.isOk).toEqual(true);
     expect(Object.keys(res.body)).toEqual(
-      expect.arrayContaining(["isOk", "comments", "pagination"])
+      expect.arrayContaining(["isOk", "comments"])
     );
     expect(res.body.comments[0].isPostAuthor).toEqual(true);
   });
@@ -261,6 +261,75 @@ describe("댓글 통합 테스트", () => {
     await connection.collection("users").deleteMany({});
     await connection.collection("posts").deleteMany({});
     await connection.collection("comments").deleteMany({});
-    await db.disconnect();
+  });
+});
+
+describe("답글 있는 댓글 삭제 테스트", () => {
+  const connection = db.createConnection(`${configs.mongoUri}/coderland`);
+  let token = "Bearer ";
+  let postId: string;
+  let commentId: string;
+
+  beforeAll(async () => {
+    await connection.collection("users").insertOne({
+      googleId: "01010203040404",
+      nickname: "testuser2",
+      name: "family given2",
+      profile: "profile photo url2",
+      grade: 1,
+    });
+
+    const user = <IUserDocument>await connection.collection("users").findOne({
+      googleId: "01010203040404",
+    });
+
+    token += createToken(user);
+  });
+
+  it("답글 있는 댓글 삭제", async () => {
+    // 포스트 생성
+    const title = "to test comment title";
+    const contents = "new contents";
+    const subject = "article";
+    const category = "none";
+    const res = await request(server)
+      .post("/api/posts")
+      .set("authorization", token)
+      .send({ title, contents, subject, category });
+    postId = res.body.postId;
+
+    // 댓글 생성
+    const res1 = await request(server)
+      .post(`/api/posts/${postId}/comments`)
+      .set("authorization", token)
+      .send({ contents: "comment contents" });
+    commentId = res1.body.commentId;
+
+    // 답글 생성
+    await request(server)
+      .post(`/api/posts/${postId}/replies`)
+      .set("authorization", token)
+      .send({ commentId, contents });
+
+    await request(server)
+      .delete(`/api/posts/${postId}/comments`)
+      .set("authorization", token)
+      .send({ commentId });
+
+    const res2 = await request(server)
+      .get(`/api/posts/${postId}/comments`)
+      .send();
+    expect(res2.body.comments[0].author).toEqual(null);
+    expect(res2.body.comments[0].contents).toEqual(
+      "작성자에 의해 삭제된 댓글입니다."
+    );
+    expect(res2.body.comments[0].isDeleted).toEqual(true);
+    expect(res2.body.comments[0].replies.length).toEqual(1);
+  });
+
+  afterAll(async () => {
+    await connection.collection("users").deleteMany({});
+    await connection.collection("posts").deleteMany({});
+    await connection.collection("comments").deleteMany({});
   });
 });
