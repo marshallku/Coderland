e3150bde0b51f7bbb02ab8032b34706f8aaeb11f
구영표 <marshallkelice@gmail.com>
2022-02-12 22:35:06 +0900

    [Fix] createExcerpt 로직 수정

--- backend/src/utils/create-excerpt.ts
+++ backend/src/utils/create-excerpt.ts
@@ -2,11 +2,15 @@ const EXCERPT_MAX_LENGTH = 50;
 
 export default function createExcerpt(contents: string, excerpt: string) {
   if (!excerpt) {
-    return contents.slice(EXCERPT_MAX_LENGTH);
+    if (contents.length > EXCERPT_MAX_LENGTH) {
+      return contents.slice(0, EXCERPT_MAX_LENGTH);
+    }
+
+    return contents.slice(0, EXCERPT_MAX_LENGTH);
   }
 
   if (excerpt.length > EXCERPT_MAX_LENGTH) {
-    return excerpt.slice(EXCERPT_MAX_LENGTH);
+    return excerpt.slice(0, EXCERPT_MAX_LENGTH);
   }
 
   return excerpt;
