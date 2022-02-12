f52aa9abab3ffd27da38562a3a3e42c7fe62fdef
구영표 <marshallkelice@gmail.com>
2022-02-13 02:37:53 +0900

    [Add] 최상단 이동 버튼 추가

---
+++ frontend/src/components/ScrollToTop.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useMemo, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { scrollTo } from "../animation/scroll";
+import formatClassName from "../utils/formatClassName";
+import { fit } from "../utils/optimizer";
+import "./ScrollToTop.css";
+/*
+
+    I Know everyone hates magic numbers,
+    but do I have to calculate the size of svg element every time user scrolls just because of that?
+
+    Just let me know if you want to modify the size of it.
+    @marshallku
+
+*/
+const SVG_SIZE = 87.9;
+
+export default function ScrollToTop() {
+  const location = useLocation();
+  const [progress, setProgress] = useState<number>(0);
+  const handleScroll = useMemo(
+    () =>
+      fit(() => {
+        setProgress(
+          (window.scrollY /
+            (document.documentElement.scrollHeight -
+              document.documentElement.clientHeight)) *
+            SVG_SIZE
+        );
+      }),
+    []
+  );
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      setProgress(0);
+      handleScroll();
+    };
+  }, [location]);
+
+  return (
+    <button
+      type="button"
+      className={formatClassName(
+        "to-top icon-north",
+        !!progress && "to-top--revealed"
+      )}
+      onClick={() => {
+        scrollTo(0);
+      }}
+    >
+      <svg
+        className="to-top__circle"
+        width="30"
+        height="30"
+        style={{
+          strokeDasharray: SVG_SIZE,
+          strokeDashoffset: `${SVG_SIZE - progress}`,
+        }}
+      >
+        <circle cx="15" cy="15" r="14" />
+      </svg>
+    </button>
+  );
+}
