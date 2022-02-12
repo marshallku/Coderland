19e333062b223c507d4105df8d717f60a9eb88c6
구영표 <marshallkelice@gmail.com>
2022-02-12 18:36:48 +0900

    Revert "[Update] 드로어 접힐시 subject 표기"

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,10 +4,28 @@ import formatClassName from "../utils/formatClassName";
 import "./Home.css";
 
 function HomePostList({ subject, isLarge }: IHomePostListProps) {
+  const subjectsInKr: {
+    // eslint-disable-next-line no-unused-vars
+    [key in TSubject]: string;
+  } = {
+    article: "댓글 남겨줘",
+    dev: "개발 정보",
+    recruit: "채용 정보",
+    chat: "잡담",
+    review: "후기 / 회고",
+    gather: "팀원 모집",
+    study: "스터디 모집",
+    code: "모각코 모집",
+    team: "팀 모집",
+  };
+
   return (
     <section
       className={formatClassName("home-posts", isLarge && "home-posts--large")}
     >
+      <header className="home-posts__header">
+        <h2 className="home-posts__title">{subjectsInKr[subject]}</h2>
+      </header>
       <PostList subject={subject} limit={3} preventPaginate />
       <footer className="home-posts__footer">
         <Link to={`/${subject}`}>더 보기</Link>
