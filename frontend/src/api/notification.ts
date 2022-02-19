d5460da7919d34e19bb1025422880e4771235983
구영표 <marshallkelice@gmail.com>
2022-02-19 10:03:59 +0000

    [Update] PWA로 업데이트

--- frontend/src/api/notification.ts
+++ frontend/src/api/notification.ts
@@ -1,7 +1,29 @@
 import instance from "./instance";
 
-export default function getNotification(): Promise<
+export function getNotification(): Promise<
   INotificationResponse | IFailResponse
 > {
   return instance.get("/users/notification");
+}
+
+export function addPushEndPoint(
+  subscription: PushSubscription
+): Promise<ISuccessResponse | IFailResponse> {
+  return instance.post("/users/push", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ subscription }),
+  });
+}
+
+export function removePushPoint(
+  subscription: PushSubscription
+): Promise<ISuccessResponse | IFailResponse> {
+  return instance.delete("/users/push", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ subscription }),
+  });
 }
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,4 +2,5 @@
 interface ImportMetaEnv {
   readonly VITE_API_SERVER_URI: string;
   readonly VITE_IMAGE_SERVER_URI: string;
+  readonly VITE_APPLICATION_SERVER_KEY: string;
 }
