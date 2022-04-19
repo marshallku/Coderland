ed2365d3e1b910ab8a0c271f78fb417df7e79974
marshallku <marshall@kakao.com>
2022-04-19 18:20:25 +0900

    [Fix] theme 초기값 수정

--- frontend/src/store/useThemeStore.ts
+++ frontend/src/store/useThemeStore.ts
@@ -1,7 +1,7 @@
 import create from "zustand";
 
 const useThemeStore = create<ThemeStore>((set) => ({
-  theme: "light",
+  theme: document.documentElement.classList.contains("dark") ? "dark" : "light",
   toggleTheme: () => {
     set((x) => {
       const nextTheme = x.theme === "dark" ? "light" : "dark";
